d5eaf8c5d2ffe34dfb117714811ffd405ef00640
varlesh <varlesh@gmail.com>
2017-06-23 20:49:09 +0400

    Scripts

--- README.md
+++ README.md
@@ -2,3 +2,21 @@
 <p align="center">
   <img src="https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-claws-mail-theme/master/preview.png" alt="Preview Papirus Themes Claws Mail"/>
 </p>
+
+## Installation
+
+### Papirus Installer
+
+Use the scripts to install the latest version directly from this repo (independently on your distro):
+
+#### Install / Update
+
+```
+wget -qO- https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-claws-mail-theme/master/install-papirus-root.sh | sh
+```
+
+#### Remove
+
+```
+wget -qO- https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-claws-mail-theme/master/remove-papirus.sh | sh
+```
---
+++ remove-papirus.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+set -e
+
+gh_repo="papirus-claws-mail-theme"
+gh_desc="Papirus icons for Claws Mail"
+
+cat <<- EOF
+
+
+
+      ppppp                         ii
+      pp   pp     aaaaa   ppppp          rr  rrr   uu   uu     sssss
+      ppppp     aa   aa   pp   pp   ii   rrrr      uu   uu   ssss
+      pp        aa   aa   pp   pp   ii   rr        uu   uu      ssss
+      pp          aaaaa   ppppp     ii   rr          uuuuu   sssss
+                          pp
+                          pp
+
+
+  $gh_desc
+  https://github.com/PapirusDevelopmentTeam/$gh_repo
+
+
+EOF
+
+echo "=> Removing $gh_desc ..."
+sudo rm -rf "/usr/share/claws-mail/themes/Papirus" \
+  "/usr/share/claws-mail/themes/ePapirus" \
+  "/usr/share/claws-mail/themes/Papirus-Dark" \
+  "/usr/share/claws-mail/themes/Papirus-Light"
+echo "=> Done!"
